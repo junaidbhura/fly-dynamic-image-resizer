adcfccb6b4ef8742acaa89c5316b266bbce8a099
Junaid Bhura <info@junaidbhura.com>
2017-09-18 11:58:09 +0930

    Bump version number

--- fly-dynamic-image-resizer.php
+++ fly-dynamic-image-resizer.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Fly Dynamic Image Resizer
 Description: Dynamically create image sizes on the fly!
-Version: 1.0.5
+Version: 2.0.0
 Author: Junaid Bhura
 Author URI: https://junaidbhura.com
 Text Domain: fly-images
